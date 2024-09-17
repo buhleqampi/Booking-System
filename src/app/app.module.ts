ed4ef037c5cb52b7c74125df5b31c801b2776fe2
buhleqampi <buhle3857@gmail.com>
2024-09-17 15:36:07 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { ClientComponent } from './components/client/client.component';
+import { BusinessDashboardComponent } from './components/business-dashboard/business-dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
+    BookingComponent,
+    ClientComponent,
+    BusinessDashboardComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
