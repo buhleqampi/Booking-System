e5e9381242343ab6011f2483a7c95e75dbdfa59f
buhleqampi <buhle3857@gmail.com>
2024-09-17 17:50:03 +0200

    did routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { ClientComponent } from './components/client/client.component';
+import { BusinessDashboardComponent } from './components/business-dashboard/business-dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'' , pathMatch: 'full'},
+  {path : 'home', component: LandingpageComponent},
+  {path : 'booking', component: BookingComponent},
+  {path : 'clients', component: ClientComponent},
+  {path : 'dashboard',component:BusinessDashboardComponent},
+  {path : 'login', component: LoginComponent},
+  {path : 'register',component:RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
