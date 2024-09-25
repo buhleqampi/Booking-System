39e9a6603beb3ab9a54fb79a5c410e506f9fd4af
buhleqampi <buhle3857@gmail.com>
2024-09-25 08:24:10 +0200

    fullcalendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { TestCompComponent } from './test-comp/test-comp.component';
 import { StaffComponent } from './components/staff/staff.component';
+import { CalenderComponent } from './components/calender/calender.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { StaffComponent } from './components/staff/staff.component';
     LoginComponent,
     RegisterComponent,
     TestCompComponent,
-    StaffComponent
+    StaffComponent,
+    CalenderComponent
   ],
   imports: [
     BrowserModule,
