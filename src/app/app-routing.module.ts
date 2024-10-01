64c668758d38a53bda3104a3f825fcbf810e221f
sipho02 <siphojunior569@gmail.comsipho02>
2024-10-01 16:41:39 +0200

    component done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ClientComponent } from './components/client/client.component';
 import { BusinessDashboardComponent } from './components/business-dashboard/business-dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HaircutsComponent } from './components/haircuts/haircuts.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'' , pathMatch: 'full'},
@@ -15,6 +16,7 @@ const routes: Routes = [
   {path : 'dashboard',component:BusinessDashboardComponent},
   {path : 'login', component: LoginComponent},
   {path : 'register',component:RegisterComponent},
+  {path : 'haircut',component:HaircutsComponent},
 ];
 
 @NgModule({
