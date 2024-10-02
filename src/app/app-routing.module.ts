50719797e6bbce88b9c767dd8533d987ca110073
buhleqampi <buhle3857@gmail.com>
2024-10-02 15:44:30 +0200

    changes done by Buhle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingpageComponent } from './components/landingpage/landingpage.component';
-import { BookingComponent } from './components/booking/booking.component';
-import { ClientComponent } from './components/client/client.component';
-import { BusinessDashboardComponent } from './components/business-dashboard/business-dashboard.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { StaffComponent } from './components/staff/staff.component';
 import { BookingsComponent } from './components/bookings/bookings.component';
 import { Test01Component } from './test01/test01.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'bookings' , pathMatch: 'full'},
-  {path : 'home', component: LandingpageComponent},
-  {path : 'booking', component: BookingComponent},
-  {path : 'clients', component: ClientComponent},
-  {path : 'dashboard',component:BusinessDashboardComponent},
-  {path : 'login', component: LoginComponent},
-  {path : 'register',component:RegisterComponent},
-  {path : 'staff',component:StaffComponent},
   {path : 'bookings',component:BookingsComponent},
   {path : 'test1',component:Test01Component},
   
