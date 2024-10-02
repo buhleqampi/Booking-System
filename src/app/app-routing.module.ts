6535aa662e8d232e8bc1037dd61bf2a647e17e46
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-10-02 22:11:45 +0200

    route to service details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookingsComponent } from './components/bookings/bookings.component';
 import { Test01Component } from './test01/test01.component';
+import { SingleServiceComponent } from './components/single-service/single-service.component';
+import { ServiceComponent } from './components/service/service.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'bookings' , pathMatch: 'full'},
   {path : 'bookings',component:BookingsComponent},
   {path : 'test1',component:Test01Component},
+  {path: 'service', component:ServiceComponent},
+  {path: 'service/:id', component: SingleServiceComponent},
   
 
 ];
