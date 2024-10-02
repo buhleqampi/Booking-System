04cc5f9802aac06a501ff0af9daa572bbcd2f7c1
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-10-02 22:12:21 +0200

    get service by id

--- src/app/services/service/service.service.ts
+++ src/app/services/service/service.service.ts
@@ -8,13 +8,17 @@ import { Service } from '../../interfaces/services';
   providedIn: 'root',
 })
 export class ServiceService {
-    private apiUrl = 'http://localhost:3000/api/services/all/';
+    private servicesEndpoint = 'http://localhost:3000/api/services/';
 
     constructor(private http: HttpClient) {}
 
     getServices(): Observable<Service[]> {
-        return this.http.get<{ services: Service[] }>(this.apiUrl).pipe(
+        return this.http.get<{ services: Service[] }>(`${this.servicesEndpoint}all/`).pipe(
             map((response) => response.services) // Extract the services array from the API response
         );
     }
+
+    getServiceById(id: string): Observable<any> {
+        return this.http.get<Service>(`${this.servicesEndpoint}${id}`);
+      }
 }
