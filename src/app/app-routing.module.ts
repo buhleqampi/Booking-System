1593aa5ca6815eeec00669c4caf5148ce12274e9
qhama991207 <mdekaziq@gmail.com>
2024-10-02 09:44:59 +0200

    save

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,27 +4,15 @@ import { LandingpageComponent } from './components/landingpage/landingpage.compo
 import { BookingComponent } from './components/booking/booking.component';
 import { ClientComponent } from './components/client/client.component';
 import { BusinessDashboardComponent } from './components/business-dashboard/business-dashboard.component';
-<<<<<<< HEAD
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { StaffComponent } from './components/staff/staff.component';
-=======
 import { LoginComponent } from './components/login/login.component';  
->>>>>>> 974f9735d6aad614fa85d0784a01e17d89cc82f5
 
 const routes: Routes = [
-  {path: '', redirectTo:'booking' , pathMatch: 'full'},
+  {path: '', redirectTo:'home' , pathMatch: 'full'},
   {path : 'home', component: LandingpageComponent},
   {path : 'booking', component: BookingComponent},
   {path : 'clients', component: ClientComponent},
   {path : 'dashboard',component:BusinessDashboardComponent},
-<<<<<<< HEAD
-  {path : 'login', component: LoginComponent},
-  {path : 'register',component:RegisterComponent},
-  {path : 'staff',component:StaffComponent},
-=======
   {path : 'login', component: LoginComponent}
->>>>>>> 974f9735d6aad614fa85d0784a01e17d89cc82f5
 ];
 
 @NgModule({
@@ -32,4 +20,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
