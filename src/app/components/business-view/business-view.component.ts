55c13233b4ef082b761e2ba850593af22d7711c4
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-10-03 03:54:08 +0200

    update business component

--- src/app/components/business-view/business-view.component.ts
+++ src/app/components/business-view/business-view.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+// business-list.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { Business } from '../../interfaces/business';
+import { BusinessService } from '../../services/business/business.service';
+import { Router } from '@angular/router'; // Import Router for navigation
 
 @Component({
   selector: 'app-business-view',
   templateUrl: './business-view.component.html',
-  styleUrl: './business-view.component.css'
+  styleUrls: ['./business-view.component.css']
 })
-export class BusinessViewComponent {
+export class BusinessViewComponent implements OnInit {
+  businesses: any = [];
 
+  constructor(private businessService: BusinessService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.fetchBusinesses();
+  }
+
+  fetchBusinesses(): void {
+    this.businessService.getAllBusinesses().subscribe(
+      (data: any) => {
+        this.businesses = data.businesses;
+      },
+      (error) => {
+        console.error('Error fetching businesses:', error);
+      }
+    );
+  }
+
+
+  goToServices(businessId: any): void {
+    
+    this.router.navigate(['/business', businessId, 'services']);
+  }
 }
