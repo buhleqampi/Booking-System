f9172b57b7d814a31ed626acc0dc64d91329a818
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-10-03 03:53:40 +0200

    change landing to businesses

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { BookingsComponent } from './components/bookings/bookings.component';
 import { Test01Component } from './test01/test01.component';
 import { SingleServiceComponent } from './components/single-service/single-service.component';
 import { ServiceComponent } from './components/service/service.component';
+import { BusinessViewComponent } from './components/business-view/business-view.component';
 
 const routes: Routes = [
-  {path: '', redirectTo:'service' , pathMatch: 'full'},
+  {path: '', redirectTo:'business' , pathMatch: 'full'},
   {path: 'service', component:ServiceComponent},
   {path: 'service/:id', component: SingleServiceComponent},
+  {path: 'business', component: BusinessViewComponent},
+  { path: 'business/:id/services', component: ServiceComponent },
   
 
 ];
